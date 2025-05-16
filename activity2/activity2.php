6256591f2386c5393fab33e9b551fec95964d7c5
TJ <132363388+TJYumul@users.noreply.github.com>
2025-05-16 10:17:18 +0800

    Add files via upload

---
+++ activity2/activity2.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Activity 2</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+        <h1>Activity 2</h1>
+        <div class="container-fluid">
+            <a href="#">Messages<span class="badge">10</span></a><br/>
+            <a href="#">Notifications<span class="badge">30</span></a><br/>
+            <button class="btn btn-primary">Click to see notifications<span class="badge">500</span></button>
+            <div class="page-header">
+                <h3>
+                    Download <span class="label label-default">Version 10.1</span>
+                </h3>
+            </div>
+        </div>
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <div class="alert alert-danger fade in">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                Record Already Exist!!!
+            </div>
+
+            <div class="alert alert-success">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                Record Already Exist!!!
+            </div>
+
+            <div class="alert alert-warning">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                Record Already Exist!!!
+            </div>
+
+            <div class="alert alert-info">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+                Record Already Exist!!!
+            </div>
+            
+
+        </div>
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <div class="jumbotron">
+                <h1>Heading in Jumbotron</h1>
+            </div>
+            <h1>Heading without Jumbotron</h1>
+
+            <div classname="page-header">
+                <h1>Welcome</h1>
+            </div>
+
+            <p class="well well-sm">This is the well</p>
+            <p class="well ">This is the well</p>
+            <p class="well well-lg">This is the well</p>
+            
+        </div>
+
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <span class="glyphicon glyphicon-trash "></span>:Trashcan <br/>
+            <span class="glyphicon glyphicon-pencil "></span>:Pencil <br/>
+            <button class="btn btn-success"><span class="glyphicon glyphicon-search"></span>Find</button>
+        </div>
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                <ul class="dropdown-menu dropdown-right">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+            <div class="dropup">
+                <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
+                <ul class="dropdown-menu">
+                    <li><a href="#">CCS</a></li>
+                    <li><a href="#">CET</a></li>
+                    <li><a href="#">CBA</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Exit</li>
+                    <li><a href="#">Logout</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+        <hr/>
+        <br/>
+        <br/>
+        <div class="container-fluid">
+            <button class="btn btn-default">Click Me</button>
+            <button class="btn btn-primary btn-xs">Click Me</button>
+            <button class="btn btn-info btn-lg">Click Me</button>
+            <button class="btn btn-success btn-md">Click Me</button>
+            <button class="btn btn-warning">Click Me</button>
+            <button class="btn btn-danger btn-sm">Click Me</button>
+            <button class="btn btn-link">Click Me</button>
+            <button class="btn btn-danger btn-block">Click Me</button>
+            <button class="btn btn-danger disabled">Click Me</button>
+            <button class="btn btn-danger active">Click Me</button>
+
+            <div class="btn-group">
+                <button class="btn btn-info">Home</button>
+                <button class="btn btn-info">About us</button> 
+                <button class="btn btn-info">Contact us</button> 
+                <button class="btn btn-info">Product</button> 
+            </div>
+
+            <div class="btn-group-vertical">
+                <button class="btn btn-info">Home</button>
+                <button class="btn btn-info">About us</button> 
+                <button class="btn btn-info">Contact us</button> 
+                <button class="btn btn-info">Product</button> 
+            </div>
+
+            <div class="btn-group btn-group-justified">
+                <div class="btn-group">
+                <button class="btn btn-success">Home</button>
+                </div>
+
+                <div class="btn-group">
+                <button class="btn btn-success">About us</button>
+                </div>
+
+                <div class="btn-group">
+                <button class="btn btn-success">Contact us</button>
+                </div>
+
+                <div class="btn-group">
+                <button class="btn btn-success">Products</button>
+                </div>
+
+            </div>
+        </div>
+
+    <hr/>
+    <br/>
+    <div class="container-fluid">
+        <img src="image/luffy.jpg">
+        <img src="image/luffy.jpg" class="img-rounded">
+        <img src="image/luffy.jpg" class="img-circle">
+        <img src="image/luffy.jpg" class="img-thumbnail"> 
+
+    </div>
+</body>
+</html>
