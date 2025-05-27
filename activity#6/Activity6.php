78ec9e1e70290b6693806ffb2b44b799020680bc
TJ <132363388+TJYumul@users.noreply.github.com>
2025-05-27 09:41:20 +0800

    Add files via upload

---
+++ activity#6/Activity6.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Activity 6  </title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<!-- jQuery  -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <style>
+        section{
+                width:100vw;
+                height: 100vw;
+                padding-top: 50px;
+            }
+        h1 {
+                padding: 0;
+                margin: 0;
+                color: white;
+            }
+    </style>
+</head>
+    <h1>Activity 6</h1>
+
+    <body data-spy="scroll" data-target=".navbar">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <ul class="nav navbar-nav">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#products">PRODUCTS</a></li>
+                <li><a href="#contact">CONTACT US</a></li>
+            </ul>
+        </nav>
+
+        <nav class="navbar navbar-inverse navbar-fixed-bottom">
+            <ul class="nav navbar-nav">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#products">PRODUCTS</a></li>
+                <li><a href="#contact">CONTACT US</a></li>
+            </ul>
+        </nav>
+
+        <section id="home" style="background-color: green;">
+            <h1>HOME</h1>
+        </section>
+
+        <section id="about" style="background-color: blue;">
+            <h1>ABOUT US</h1>
+        </section>
+
+        <section id="products" style="background-color: gray;">
+            <h1>PRODUCTS</h1>
+        </section>
+
+        <section id="contact" style="background-color: yellow;">
+            <h1>CONTACT US</h1>
+        </section>
+
+    </body>
+</html>
